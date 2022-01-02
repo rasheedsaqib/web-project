61d414294105f38dc473b56f52a8a88565000fee
Sharryy <ibneadam388@gmail.com>
2022-01-02 23:57:23 +0500

    Changes

--- resources/views/front/product.blade.php
+++ resources/views/front/product.blade.php
@@ -7,10 +7,11 @@
                 <h3 class="text-center">Categories</h3>
                 <div class="cat-aside-wrap">
                     @foreach ($getcategory as $category)
-                    <a href="{{URL::to('/product/'.$category->id)}}" class="cat-check border-top-no @if (request()->id == $category->id) active @endif">
-                        <img src='{!! asset("public/images/category/".$category->image) !!}' alt="">
-                        <p>{{$category->category_name}}</p>
-                    </a>
+                        <a href="{{URL::to('/product/'.$category->id)}}"
+                           class="cat-check border-top-no @if (request()->id == $category->id) active @endif">
+                            <img src='{!! asset("images/category/".$category->image) !!}' alt="">
+                            <p>{{$category->category_name}}</p>
+                        </a>
                     @endforeach
                 </div>
             </div>
@@ -28,38 +29,39 @@
                 </div>
                 <div class="row">
                     @foreach ($getitem as $item)
-                    <div class="col-xl-4 col-md-6">
-                        <div class="pro-box">
-                            <div class="pro-img">
-                                <a href="{{URL::to('product-details/'.$item->id)}}">
-                                    <img src='{{$item["itemimage"]->image }}' alt="">
-                                </a>
-                                @if (Session::get('id'))
-                                    @if ($item->is_favorite == 1)
-                                        <i class="fas fa-heart i"></i>
-                                    @else
-                                        <i class="fal fa-heart i" onclick="MakeFavorite('{{$item->id}}','{{Session::get('id')}}')"></i>
-                                    @endif
-                                @endif
-                            </div>
-                            <div class="product-details-wrap">
-                                <div class="product-details">
+                        <div class="col-xl-4 col-md-6">
+                            <div class="pro-box">
+                                <div class="pro-img">
                                     <a href="{{URL::to('product-details/'.$item->id)}}">
-                                        <h4>{{$item->item_name}}</h4>
+                                        <img src='{{$item["itemimage"]->image }}' alt="">
                                     </a>
-                                    <p class="pro-pricing">{{$getdata->currency}}{{number_format($item->item_price, 2)}}</p>
+                                    @if (Session::get('id'))
+                                        @if ($item->is_favorite == 1)
+                                            <i class="fas fa-heart i"></i>
+                                        @else
+                                            <i class="fal fa-heart i"
+                                               onclick="MakeFavorite('{{$item->id}}','{{Session::get('id')}}')"></i>
+                                        @endif
+                                    @endif
                                 </div>
-                                <div class="product-details">
-                                    <p>{{ Str::limit($item->item_description, 60) }}</p>
+                                <div class="product-details-wrap">
+                                    <div class="product-details">
+                                        <a href="{{URL::to('product-details/'.$item->id)}}">
+                                            <h4>{{$item->item_name}}</h4>
+                                        </a>
+                                        <p class="pro-pricing">{{$getdata->currency}}{{number_format($item->item_price, 2)}}</p>
+                                    </div>
+                                    <div class="product-details">
+                                        <p>{{ Str::limit($item->item_description, 60) }}</p>
                                     <!-- @if (Session::get('id'))
                                         <button class="btn" onclick="AddtoCart('{{$item->id}}','{{Session::get('id')}}')">Add to Cart</button>
                                     @else
                                         <a class="btn" href="{{URL::to('/signin')}}">Add to Cart</a>
                                     @endif -->
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
                     @endforeach
                 </div>
                 {!! $getitem->links() !!}
